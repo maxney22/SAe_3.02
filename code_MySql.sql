997d36daa460e348db8ae529f074a1ec746fc2e9
maxney22 <maxime.ney2206@gmail.com>
2025-12-30 01:07:08 +0100

    Code Mysql pour la BDD

--- code_MySql.sql
+++ code_MySql.sql
@@ -1,11 +1,6 @@
 CREATE DATABASE sae302;
-
 CREATE USER 'maxgui'@'%' IDENTIFIED BY 'toto';
 GRANT ALL PRIVILEGES ON sae302.* TO 'maxgui'@'%';
-
 FLUSH PRIVILEGES;
-
 USE sae302;
-DROP TABLE routeur;
-
 CREATE TABLE routeur (nom VARCHAR(50) PRIMARY KEY, adresse_ip VARCHAR(20), port INT,cle_pub_n TEXT, cle_pub_e TEXT, ping DATETIME);
